260d9b6d758a9ba2d0622347f6f2d3baf38e7352
ManApart <akucera@pillartechnology.com>
2019-06-26 13:40:48 -0400

    remove unneeded code in helpers

--- lib/smart_city/helpers.ex
+++ lib/smart_city/helpers.ex
@@ -38,21 +38,6 @@ defmodule SmartCity.Helpers do
   def to_atom_keys(value), do: value
 
   @doc """
-  Creates a map with all nil values that fufills a given schema
-  """
-  # def empty_map_from_schema(schema, acc \\ %{}) do
-  #   Enum.reduce(schema, acc, fn field, acc -> put_nil_field(field, acc) end)
-  # end
-
-  # defp put_nil_field(%{name: name, subSchema: subSchema}, acc) do
-  #   Map.put(acc, String.to_atom(name), empty_map_from_schema(subSchema, %{}))
-  # end
-
-  # defp put_nil_field(field, acc) do
-  #   Map.put(acc, String.to_atom(field.name), nil)
-  # end
-
-  @doc """
   Merges two maps into one, including sub maps. Matching keys from the right map will override their corresponding key in the left map.
   """
   @spec deep_merge(map(), map()) :: map()
